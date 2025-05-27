1b9f30a11914da34404e4838d6a1ab8b72885442
alisya02 <alisyazh4ra@gmail.com>
2025-05-27 09:48:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("Selamat Datang di Web Cinai")
 st.write(
-    "ngoding seru bersama Alisya Naila"
+    "ngodingseru bersama Alisya Naila"
 )
 st.image("yey 048.JPG", widht=200)
